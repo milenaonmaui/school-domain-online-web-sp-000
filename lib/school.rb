2f311a20c987ffea9671d0b6e3c185ebccdf58d8
Milena Kari <milenakari@yahoo.com>
2019-01-08 20:25:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,9 +8,13 @@ end
 def roster
   @roster
 end
+<<<<<<< HEAD
 def roster=(roster)
   @roster=roster
 end
+=======
+
+>>>>>>> 95b1585924d4a1010ec1399057510d6c5b557d9f
 def add_student(name, grade)
    if @roster[grade] != nil
      @roster[grade] << name
@@ -21,12 +25,21 @@ def add_student(name, grade)
   def grade(grade_num)
     @roster[grade_num]
   end
+<<<<<<< HEAD
 
   def sort
     @roster.each do |grade, students|
       students.sort!
     end
     roster
+=======
+  def sort
+    @roster = @roster.sort
+    @roster.each do |grade, students|
+      @roster[grade.to_sym] = students.sort
+    end
+    @roster
+>>>>>>> 95b1585924d4a1010ec1399057510d6c5b557d9f
   end
 end
 end
